21c5c7877a4620ec1d50071f1ef2a14b62796efe
Nilo <niloarnt@gmail.com>
2021-06-14 21:17:48 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h2>This is Nilo Que legal caraio</h2>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
