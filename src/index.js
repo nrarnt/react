727803fda5f45ce70719c0056c645a34a73f9068
Nilo <niloarnt@gmail.com>
2021-06-15 22:00:42 -0300

    Lista de Livros

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h2>This is Nilo Que legal caraio</h2>;
+//stateless functional component
+//always return JSX
+
+//JSX rules
+//className instead of class
+//close every element
+//return only one element
+
+function Booklist() {
+  return (
+    <section>
+      <Livro />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Livro = () => {
+  return (
+    <article>
+      <Capa></Capa>
+      <Titulo></Titulo>
+      <Autor></Autor>
+    </article>
+  );
+};
+
+const Capa = () => (
+  <img src="https://m.media-amazon.com/images/I/512ERYcB18L.jpg" alt="" />
+);
+
+const Titulo = () => <h1>O guia do mochileiro das galáxias</h1>;
+
+const Autor = () => <h4>Douglas Adams</h4>;
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
